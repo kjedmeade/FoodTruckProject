37d85b0910334eb67a79ae7c02520958047c5598
Kayalyn Edmeade <kjedmeade@gmail.com>
2020-09-06 10:08:32 -0700

    first commit

---
+++ src/com/skilldistillery/foodproject/FoodTruck.java
@@ -0,0 +1,15 @@
+package com.skilldistillery.foodproject;
+
+public class FoodTruck {
+	private String name;
+	private String foodtype;
+	private int rating;
+	private int totalNumTrucks=5;
+
+	
+	public foodTruck(String n, String f, int r ) {
+		name = n;
+		foodType = f;
+		rating=r;
+	}
+}
