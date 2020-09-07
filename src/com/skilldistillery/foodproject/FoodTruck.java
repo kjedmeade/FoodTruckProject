104306a45ff239f705ec02766f9134c20ea507a3
Kayalyn Edmeade <kjedmeade@gmail.com>
2020-09-06 18:57:38 -0700

    fourth commit

--- src/com/skilldistillery/foodproject/FoodTruck.java
+++ src/com/skilldistillery/foodproject/FoodTruck.java
@@ -4,26 +4,21 @@ public class FoodTruck {
 	public String name;
 	public String foodType;
 	public int rating;
-	public int totalNumTrucks=1;
-	public static int nextTruckID=1;
-	
-	
+	public int totalNumTrucks = 1;
+	public static int nextTruckID = 1;
 
-	
 	public FoodTruck(String n, String f, int r, int nextTruckID) {
 		name = n;
 		foodType = f;
-		rating=r;
-		nextTruckID=1;
+		rating = r;
+		nextTruckID = 1;
 		nextTruckID++;
-		
+
 	}
-	
-		
+
 	public String getName() {
 		return name;
 	}
-
 
 	public String getFoodType() {
 		return foodType;
@@ -32,15 +27,19 @@ public class FoodTruck {
 	public int getRating() {
 		return rating;
 	}
+
+	public static int getNextTruckID() {
+		return nextTruckID;
+	}
 	
+	public void displayFoodTrucks(FoodTruck [] x) {
+		String x = this.toString();
+		System.out.println(x);
+	}
+	@Override
 	public String toString() {
 		return "FoodTruck [name=" + name + ", foodType=" + foodType + ", rating=" + rating + ", totalNumTrucks="
 				+ totalNumTrucks + "]";
 	}
 
-
-	}
-
-
-	
-
+}
