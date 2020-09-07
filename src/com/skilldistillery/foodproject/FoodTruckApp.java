4bc82021ac6812eea01364f0b03a6230f89ec2e8
Kayalyn Edmeade <kjedmeade@gmail.com>
2020-09-07 14:03:22 -0700

    seventh commit

--- src/com/skilldistillery/foodproject/FoodTruckApp.java
+++ src/com/skilldistillery/foodproject/FoodTruckApp.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class FoodTruckApp {
 
+	
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
@@ -27,9 +28,9 @@ public class FoodTruckApp {
 			if (input1.equals("quit")) {
 				break;
 			}
-			System.out.println("Foodtype;");
+			System.out.println("Foodtype");
 			String input2 = sc.next();
-			System.out.println("Rating (1-10):");
+			System.out.println("Rating (1-10)");
 			int input3 = sc.nextInt();
 			FoodTruck ft = new FoodTruck(input1, input2, input3);
 			allFoodTrucks[i] = ft;
